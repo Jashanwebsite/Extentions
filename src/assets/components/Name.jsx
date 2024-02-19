0f833b184e47f98af8106394003855e9de4a33f7
Jashanwebsite <jashandeepsingh61124@gmail.com>
2024-02-19 23:56:51 +0530

    changess

--- src/assets/components/Name.jsx
+++ src/assets/components/Name.jsx
@@ -4,146 +4,146 @@ import Clock from "./Clock";
 import Clock2 from "./Clock2";
 
 function Name() {
-  const [animation, setanimation] = useState(false);
+  // const [animation, setanimation] = useState(false);
     
-  useEffect(() => {
-    const starting = setInterval(() => {
-      const j = document.getElementById("j");
-      const a = document.getElementById("a");
-      const s = document.getElementById("s");
-      const h = document.getElementById("h");
-      const last_a = document.getElementById("last_a");
-      const n = document.getElementById("n");
+  // useEffect(() => {
+  //   const starting = setInterval(() => {
+  //     const j = document.getElementById("j");
+  //     const a = document.getElementById("a");
+  //     const s = document.getElementById("s");
+  //     const h = document.getElementById("h");
+  //     const last_a = document.getElementById("last_a");
+  //     const n = document.getElementById("n");
 
-      if (animation) {
-        j.classList.toggle("animation");
-        if (j.classList.contains("doneanimation")) {
-          h.classList.toggle("animation");
-        }
-      }
-    }, 4);
-    // for done
-    const mid = setInterval(() => {
-      const j = document.getElementById("j");
-      const a = document.getElementById("a");
-      const s = document.getElementById("s");
-      const h = document.getElementById("h");
-      const last_a = document.getElementById("last_a");
-      const n = document.getElementById("n");
-      if (animation) {
-        if (!j.classList.contains("doneanimation")) {
-          return j.classList.add("doneanimation");
-        }
+  //     if (animation) {
+  //       j.classList.toggle("animation");
+  //       if (j.classList.contains("doneanimation")) {
+  //         h.classList.toggle("animation");
+  //       }
+  //     }
+  //   }, 4);
+  //   // for done
+  //   const mid = setInterval(() => {
+  //     const j = document.getElementById("j");
+  //     const a = document.getElementById("a");
+  //     const s = document.getElementById("s");
+  //     const h = document.getElementById("h");
+  //     const last_a = document.getElementById("last_a");
+  //     const n = document.getElementById("n");
+  //     if (animation) {
+  //       if (!j.classList.contains("doneanimation")) {
+  //         return j.classList.add("doneanimation");
+  //       }
 
-        if (
-          j.classList.contains("doneanimation") &&
-          !h.classList.contains("doneanimation") &&
-          !a.classList.contains("doneanimation")
-        ) {
-          return setTimeout(async () => {
-            h.classList.add("doneanimation");
-          }, 0);
-        }
+  //       if (
+  //         j.classList.contains("doneanimation") &&
+  //         !h.classList.contains("doneanimation") &&
+  //         !a.classList.contains("doneanimation")
+  //       ) {
+  //         return setTimeout(async () => {
+  //           h.classList.add("doneanimation");
+  //         }, 0);
+  //       }
 
-        if (
-          h.classList.contains("doneanimation") &&
-          !a.classList.contains("doneanimation")
-        ) {
-          const returnanimation = () => {
-            a.classList.add("doneanimation");
-            last_a.classList.add("doneanimation");
-            console.log("returnanimation");
-          };
-          return setTimeout(returnanimation, 0);
-        }
-        if (
-          !s.classList.contains("doneanimation") &&
-          a.classList.contains("doneanimation")
-        ) {
-          const returnanimation = () => {
-            s.classList.add("doneanimation");
-            n.classList.add("doneanimation");
-            console.log("returnanimation");
-          };
-          return setTimeout(returnanimation, 0);
-        }
-      }
-    }, 6000);
+  //       if (
+  //         h.classList.contains("doneanimation") &&
+  //         !a.classList.contains("doneanimation")
+  //       ) {
+  //         const returnanimation = () => {
+  //           a.classList.add("doneanimation");
+  //           last_a.classList.add("doneanimation");
+  //           console.log("returnanimation");
+  //         };
+  //         return setTimeout(returnanimation, 0);
+  //       }
+  //       if (
+  //         !s.classList.contains("doneanimation") &&
+  //         a.classList.contains("doneanimation")
+  //       ) {
+  //         const returnanimation = () => {
+  //           s.classList.add("doneanimation");
+  //           n.classList.add("doneanimation");
+  //           console.log("returnanimation");
+  //         };
+  //         return setTimeout(returnanimation, 0);
+  //       }
+  //     }
+  //   }, 6000);
 
-    return () => {
-      clearInterval(starting);
-      clearInterval(mid);
-    };
-  }, [animation]);
-  //  for handeling the a and last_a ani mation -----------------------------------------------------------------------------------------------------
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      const a = document.getElementById("a");
-      const h = document.getElementById("h");
-      const last_a = document.getElementById("last_a");
-      if (animation) {
-        if (
-          h.classList.contains("doneanimation") &&
-          !a.classList.contains("doneanimation")
-        ) {
-          last_a.classList.toggle("animation");
-          a.classList.toggle("animation");
-          console.log("a dimming start");
-        }
-      }
-    }, 300);
-    return () => clearInterval(interval);
-  }, [animation]);
-  //  for handeling the s and n ani mation -----------------------------------------------------------------------------------------------------
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const s = document.getElementById("s");
-      const n = document.getElementById("n");
-      const a = document.getElementById("a");
-      if (animation) {
-        if (
-          a.classList.contains("doneanimation") &&
-          !s.classList.contains("doneanimation")
-        ) {
-          s.classList.toggle("animation");
-          n.classList.toggle("animation");
-          console.log("s dimming start");
-        }
-      }
-      return () => clearInterval(interval);
-    }, 10);
-  }, [animation]);
-  // for resetting animation
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const j = document.getElementById("j");
-      const a = document.getElementById("a");
-      const s = document.getElementById("s");
-      const h = document.getElementById("h");
-      const last_a = document.getElementById("last_a");
-      const n = document.getElementById("n");
+  //   return () => {
+  //     clearInterval(starting);
+  //     clearInterval(mid);
+  //   };
+  // }, [animation]);
+  // //  for handeling the a and last_a ani mation -----------------------------------------------------------------------------------------------------
+  // useEffect(() => {
+  //   const interval = setInterval(async () => {
+  //     const a = document.getElementById("a");
+  //     const h = document.getElementById("h");
+  //     const last_a = document.getElementById("last_a");
+  //     if (animation) {
+  //       if (
+  //         h.classList.contains("doneanimation") &&
+  //         !a.classList.contains("doneanimation")
+  //       ) {
+  //         last_a.classList.toggle("animation");
+  //         a.classList.toggle("animation");
+  //         console.log("a dimming start");
+  //       }
+  //     }
+  //   }, 300);
+  //   return () => clearInterval(interval);
+  // }, [animation]);
+  // //  for handeling the s and n ani mation -----------------------------------------------------------------------------------------------------
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const s = document.getElementById("s");
+  //     const n = document.getElementById("n");
+  //     const a = document.getElementById("a");
+  //     if (animation) {
+  //       if (
+  //         a.classList.contains("doneanimation") &&
+  //         !s.classList.contains("doneanimation")
+  //       ) {
+  //         s.classList.toggle("animation");
+  //         n.classList.toggle("animation");
+  //         console.log("s dimming start");
+  //       }
+  //     }
+  //     return () => clearInterval(interval);
+  //   }, 10);
+  // }, [animation]);
+  // // for resetting animation
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const j = document.getElementById("j");
+  //     const a = document.getElementById("a");
+  //     const s = document.getElementById("s");
+  //     const h = document.getElementById("h");
+  //     const last_a = document.getElementById("last_a");
+  //     const n = document.getElementById("n");
       
-      if (animation) {
-        const lists = ["animation", "doneanimation"];
+  //     if (animation) {
+  //       const lists = ["animation", "doneanimation"];
       
-        // Iterate through the elements and remove each class from the list
-        [j, s, h, a, last_a, n].forEach(element => {
-          lists.forEach(className => {
-            element.classList.remove(className);
-          });
-        });
+  //       // Iterate through the elements and remove each class from the list
+  //       [j, s, h, a, last_a, n].forEach(element => {
+  //         lists.forEach(className => {
+  //           element.classList.remove(className);
+  //         });
+  //       });
       
-        console.log("Animations set to false");
-        setanimation(false);
-      }
-       if(!animation) {
-        console.log("aimation set to true")
-         setanimation(true);
-      }
-    }
-    , 30000);
-    return ()=>clearInterval(interval)
-  }, [animation]);
+  //       console.log("Animations set to false");
+  //       setanimation(false);
+  //     }
+  //      if(!animation) {
+  //       console.log("aimation set to true")
+  //        setanimation(true);
+  //     }
+  //   }
+  //   , 30000);
+  //   return ()=>clearInterval(interval)
+  // }, [animation]);
 
 
   //  components ---------------------------------------------------------------------
@@ -171,6 +171,7 @@ function Name() {
       <h1 className="n  " id="n">
         n
       </h1>
+      <div className="line"></div>
     </div>
     </div>
   );
